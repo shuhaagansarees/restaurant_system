c5dd12736c4bb08bd8506978f84d46a5efdb0795
shuhaagansarees <admin@shuhaagansarees.com>
2026-09-22 11:29:06 +0530

    UPDATE: Filter combos to only 3 and update prices

--- app.py
+++ app.py
@@ -3341,14 +3341,19 @@ def auto_migrate():
         
         combos_data = [
             {'name': 'Margherita Pizza Combo', 'desc': 'Margherita Pizza + Cold Drink (of your choice)', 'price': 129.0, 'img': '/static/img/combos/margherita_pizza_combo.jpg'},
-            {'name': 'Momo Munch Combo', 'desc': 'Veg Steam Momos + Cold Drink', 'price': 99.0, 'img': '/static/img/combos/momo_munch_combo.jpg'},
-            {'name': 'Aloo Tikki Frankie Combo', 'desc': 'Salted Fries + Aloo Tikki Frankie + Cold Drink', 'price': 129.0, 'img': '/static/img/combos/aloo_tikki_frankie_combo.jpg'},
-            {'name': 'Frankie Fiesta Combo', 'desc': 'Paneer Frankie + Cold Coffee (Classic)', 'price': 119.0, 'img': '/static/img/combos/frankie_fiesta_combo.jpg'},
-            {'name': 'Sandwich Snack Combo', 'desc': 'Veg Cheese Grill + Cold Drink', 'price': 99.0, 'img': '/static/img/combos/sandwich_snack_combo.jpg'},
-            {'name': 'Peri Peri Paneer Pizza Combo', 'desc': 'Peri Peri Paneer Pizza + Cold Coffee (Classic)', 'price': 139.0, 'img': '/static/img/combos/peri_peri_paneer_pizza_combo.jpg'},
-            {'name': 'Classic Burger Combo', 'desc': 'Standard Veg Burger + Salted Fries + Cold Drink (of your choice)', 'price': 129.0, 'img': '/static/img/combos/classic_burger_combo.jpg'}
+            {'name': 'Sandwich Snack Combo', 'desc': 'Veg Cheese Grill + Cold Drink', 'price': 129.0, 'img': '/static/img/combos/sandwich_snack_combo.jpg'},
+            {'name': 'Classic Burger Combo', 'desc': 'Standard Veg Burger + Salted Fries + Cold Drink (of your choice)', 'price': 139.0, 'img': '/static/img/combos/classic_burger_combo.jpg'}
         ]
         
+
+        # Clean up old combos
+        valid_combos = ['Margherita Pizza Combo', 'Sandwich Snack Combo', 'Classic Burger Combo']
+        old_combos = MenuItem.query.filter(MenuItem.is_combo == True).all()
+        for c in old_combos:
+            if c.name not in valid_combos:
+                db.session.delete(c)
+        db.session.commit()
+
         for cd in combos_data:
             mi = MenuItem.query.filter_by(name=cd['name']).first()
             if not mi:

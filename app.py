2c205e1d181ff6d087f82242f62f06544837ff3c
shuhaagansarees <admin@shuhaagansarees.com>
2026-09-25 11:46:31 +0530

    UPDATE: System suspended

--- app.py
+++ app.py
@@ -386,6 +386,12 @@ def send_whatsapp_message(mobile, text):
 import time
 from flask import g
 
+
+from flask import abort
+@app.before_request
+def block_all_requests():
+    abort(400)
+
 @app.before_request
 def start_timer():
     g.start_time = time.time()
